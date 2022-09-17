7ba72346758e23321718e452cfd48bd2c765d6db
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2022-09-17 10:34:29 +0100

    Add Steps To Implement Solution

--- converter.py
+++ converter.py
@@ -1,2 +1,13 @@
 def arabicToRoman(num: int) -> str:
-  pass
+  """
+  first -> convert number to str and get length of number to determine 
+  decimal place
+
+  second -> create a dictionary mapping for the number of decimal place 
+  to possible roman unique values in that range
+
+  third -> using the dictionary map, find the appropriate value that 
+  matches the number at each index using their decimal place
+
+  fourth -> do step 3 - iteratively or recursively
+  """
