5b784846230a397640cd32f9422988db5385a554
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2022-09-17 10:36:58 +0100

    Modify Steps to Implement solution

--- converter.py
+++ converter.py
@@ -6,6 +6,9 @@ def arabicToRoman(num: int) -> str:
   second -> create a dictionary mapping for the number of decimal place 
   to possible roman unique values in that range
 
+  intermediate step -> check if length of number string is greater than 4
+  or number itself is greater than 3999, if true raise exception
+
   third -> using the dictionary map, find the appropriate value that 
   matches the number at each index using their decimal place
 
