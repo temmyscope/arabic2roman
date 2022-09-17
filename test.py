629be69af44cb7b13a100eee0321f3c8c8ceeb12
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2022-09-17 13:22:13 +0100

    Complete Code Cleanup

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ class TestArabicToRomain(unittest.TestCase):
   def test_valid_input_value(self):
     self.assertEqual( arabicToRoman(9), 'IX' )
     self.assertEqual( arabicToRoman(79), 'LXXIX' )
-    self.assertEqual( arabicToRoman(299), 'CCLXCIX' )
-    self.assertEqual( arabicToRoman(650), 'DL' )
-    self.assertEqual( arabicToRoman(1797), 'MDCCLXCVII' )
-    self.assertEqual( arabicToRoman(1789), 'MDCCLXXXVII' )
+    self.assertEqual( arabicToRoman(299), 'CCXCIX' )
+    self.assertEqual( arabicToRoman(650), 'DCL' )
+    self.assertEqual( arabicToRoman(1797), 'MDCCXCVII' )
+    self.assertEqual( arabicToRoman(1889), 'MDCCCLXXXIX' )
 
   def test_invalid_input_exception(self):
     # check that arabicToRoman function raises excepitions 
@@ -19,7 +19,7 @@ class TestArabicToRomain(unittest.TestCase):
       arabicToRoman('4000')
     # check that arabicToRoman function raises excepitions
     # for values greater than 3999
-    with self.assertRaises():
+    with self.assertRaises(ValueError):
       arabicToRoman(4000)
 
 if __name__ == '__main__':
